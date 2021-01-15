6ec27ef8588f0ed82dee0fddbef403def7b2562a
9ui <71952965+9ui@users.noreply.github.com>
2021-01-15 20:41:56 +0800

    feat: 完善首页

--- src/components/Layouts/src/LayoutHeader.vue
+++ src/components/Layouts/src/LayoutHeader.vue
@@ -1,16 +1,31 @@
 <template>
-  <div>
-    <header class="layout-header">
-      <div class="layout-header__content">
-        <nuxt-link to="/"> <img class="layout-header__logo" src="/logo.png" /></nuxt-link>
-        <BasicMenu></BasicMenu>
-      </div>
-      <div class="layout-header__action">
-        <!-- <button @click="navTo('login')">登陆</button>
-        <button type="primary">注册送会员</button> -->
-      </div>
-    </header>
-  </div>
+  <header class="text-gray-600 body-font">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+          viewBox="0 0 24 24"
+        >
+          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+        </svg>
+        <span class="ml-3 text-xl">Laihua</span>
+      </a>
+      <nav
+        class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center"
+      >
+        <a class="mr-5 hover:text-gray-900">First Link</a>
+        <a class="mr-5 hover:text-gray-900">Second Link</a>
+        <a class="mr-5 hover:text-gray-900">Third Link</a>
+        <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+      </nav>
+    </div>
+  </header>
 </template>
 <script lang="ts">
   import * as Core from '@/core';
