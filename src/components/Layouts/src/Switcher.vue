dcdca64d0f494383cf8897318c2e475ab79bda61
9ui <71952965+9ui@users.noreply.github.com>
2021-03-10 15:29:14 +0800

    feat: edit pageB

--- src/components/Layouts/src/Switcher.vue
+++ src/components/Layouts/src/Switcher.vue
@@ -2,43 +2,43 @@
   <div class="switcher">
     <div
       data-theme="indigo"
-      class="theme-button cursor-pointer bg-indigo-500"
+      class="bg-indigo-500 cursor-pointer theme-button"
       :class="{ 'is-active': currentTheme === 'indigo' }"
       @click="SET_THEME('indigo')"
     ></div>
     <div
       data-theme="yellow"
-      class="theme-button cursor-pointer bg-yellow-500"
+      class="bg-yellow-500 cursor-pointer theme-button"
       :class="{ 'is-active': currentTheme === 'yellow' }"
       @click="SET_THEME('yellow')"
     ></div>
     <div
       data-theme="red"
-      class="theme-button cursor-pointer bg-red-500"
+      class="bg-red-500 cursor-pointer theme-button"
       :class="{ 'is-active': currentTheme === 'red' }"
       @click="SET_THEME('red')"
     ></div>
     <div
       data-theme="purple"
-      class="theme-button cursor-pointer bg-purple-500"
+      class="bg-purple-500 cursor-pointer theme-button"
       :class="{ 'is-active': currentTheme === 'purple' }"
       @click="SET_THEME('purple')"
     ></div>
     <div
       data-theme="pink"
-      class="theme-button cursor-pointer bg-pink-500"
+      class="bg-pink-500 cursor-pointer theme-button"
       :class="{ 'is-active': currentTheme === 'pink' }"
       @click="SET_THEME('pink')"
     ></div>
     <div
       data-theme="blue"
-      class="theme-button cursor-pointer bg-blue-500"
+      class="bg-blue-500 cursor-pointer theme-button"
       :class="{ 'is-active': currentTheme === 'blue' }"
       @click="SET_THEME('blue')"
     ></div>
     <div
       data-theme="green"
-      class="theme-button cursor-pointer bg-green-500"
+      class="bg-green-500 cursor-pointer theme-button"
       :class="{ 'is-active': currentTheme === 'green' }"
       @click="SET_THEME('green')"
     ></div>
--- src/components/Views/page/PageB.vue
+++ src/components/Views/page/PageB.vue
@@ -1,29 +1,30 @@
 <template>
-  <section class="w-full mx-auto">
+  <section class="w-full mx-auto bg-white">
+    <!-- section 1 -->
     <div class="relative">
       <img
         src="https://soom.us/_nuxt/img/banner-bg.162e32a.jpg"
-        class="w-full absolute top-0 left-0 z-0"
+        class="absolute top-0 left-0 z-0 w-full"
       />
-      <nav class="w-full h-auto flex px-10 pt-5 absolute left-0 top-0 z-10">
+      <nav class="absolute top-0 left-0 z-10 flex w-full h-auto px-10 pt-5">
         <img src="https://soom.us/_nuxt/img/logo.e1cd00a.png" class="w-auto h-7" />
       </nav>
       <div
-        class="flex flex-col relative w-full items-center pt-36 text-center select-none container mx-auto"
+        class="container relative flex flex-col items-center w-full mx-auto text-center select-none pt-36"
       >
-        <h1 class="text-6xl leading-snug font-bold"
+        <h1 class="text-6xl font-bold leading-snug text-gray-800"
           ><span class="text-purple-700">SOOM</span> is going to transform your<br />presentation
           experience</h1
         >
-        <p class="m-9 text-xl text-gray-500 leading-9"
+        <p class="text-xl leading-9 text-gray-500 m-9"
           >Say goodbye to cut-and-dry presentations with PPT on the screen. <br />Put yourself
           inside the picture and communicate with your audience face-to-face.</p
         >
         <button
-          class="flex bg-purple-700 rounded-full py-4 px-12 items-center text-white mt-8 hover:bg-purple-800 focus:outline-none"
+          class="flex items-center px-12 py-4 mt-8 text-white bg-purple-700 rounded-full hover:bg-purple-800 focus:outline-none"
         >
           <svg
-            class="w-9 mr-3"
+            class="mr-3 w-9"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -38,18 +39,184 @@
           </svg>
           DOWNLOAD FOR WINDOWS
         </button>
-        <span class="mt-3 text-gray-400 font-light text-sm">MAC VERSION IS COMING SOON</span>
-        <div class="w-1/2 bg-gray-200 mx-auto rounded mt-28">
+        <span class="mt-3 text-sm font-light text-gray-400">MAC VERSION IS COMING SOON</span>
+        <div class="w-1/2 mx-auto bg-gray-200 rounded mt-28">
           <video
-            class="rounded w-full"
+            class="w-full rounded"
             src="https://resources.laihua.com/2021-2-7/756eba5d-cb75-4a15-b059-6ac713f8b35d.mp4"
+            autoplay
+            loop
           ></video>
         </div>
-        <div class="mx-auto mt-28 text-3xl font-normal text-gray-800 leading-10"
+        <div class="mx-auto text-2xl font-light leading-10 text-gray-800 mt-28"
           >Use Soom to connect with the video-conferencing software<br />that works best for
           you.</div
         >
-        <img class="mt-14" src="https://soom.us/_nuxt/img/dacf2c5.png" alt="" />
+        <img class="mb-32 mt-14" src="https://soom.us/_nuxt/img/partners.dacf2c5.png" alt="" />
+      </div>
+    </div>
+    <!-- section 2 -->
+    <div class="w-full overflow-hidden bg-purple-500 p-44">
+      <div class="relative">
+        <img
+          src="https://soom.us/_nuxt/img/carousel-bg-icon.55e2296.png"
+          class="absolute top-0 left-0 z-0 w-full"
+        />
+        <div class="container relative z-10 flex flex-col items-center mx-auto">
+          <div class="text-5xl text-white">Why SOOM?</div>
+          <span class="text-purple-50 m-9"
+            >With the help of SOOM, you can deliver a knock-out presentation with 5 minutes of lead
+            time.</span
+          >
+          <div class="flex items-center mt-12">
+            <img
+              class="w-1/2"
+              src="https://soom.us/_nuxt/img/feature-3.0badefb.png"
+              alt=""
+              srcset=""
+            />
+            <div class="ml-20">
+              <span class="text-3xl text-white">True-to-life virtual background</span>
+              <p class="mt-10 leading-7 text-justify text-white w-96"
+                >Just got another conference to be held on short notice? Need to use a professional
+                live shooting base? Concerned about the information privacy of videoconferencing?
+                Use Soom's virtual background with automatic image matting and complete your video
+                conference from home.</p
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- section 3 -->
+    <div class="w-full">
+      <div class="container flex flex-col items-center p-40 mx-auto">
+        <div class="text-5xl">What can I do with SOOM?</div>
+        <div class="flex flex-wrap items-center justify-center">
+          <div class="flex flex-col items-center mt-20 mr-12 text-center">
+            <div class="w-8 h-8 bg-purple-600"></div>
+            <div class="my-3 text-xl font-bold text-gray-800">Online Education</div>
+            <p class="leading-7 text-gray-800 w-60"
+              >Lift the limitations of space, time, and environment.</p
+            >
+          </div>
+          <div class="flex flex-col items-center mt-20 mr-12 text-center">
+            <div class="w-8 h-8 bg-purple-600"></div>
+            <div class="my-3 text-xl font-bold text-gray-800">Business Cooperation</div>
+            <p class="leading-7 text-gray-800 w-60"
+              >Launch video conferences to communicate with partners.</p
+            >
+          </div>
+          <div class="flex flex-col items-center mt-20 mr-12 text-center">
+            <div class="w-8 h-8 bg-purple-600"></div>
+            <div class="my-3 text-xl font-bold text-gray-800">Personnel Training</div>
+            <p class="leading-7 text-gray-800 w-60"
+              >Office type of templates help to build corporate cultures.</p
+            >
+          </div>
+          <div class="flex flex-col items-center mt-20 mr-12 text-center">
+            <div class="w-8 h-8 bg-purple-600"></div>
+            <div class="my-3 text-xl font-bold text-gray-800">Popularization of Science</div>
+            <p class="leading-7 text-gray-800 w-60"
+              >Real shots and animation help to convey information vividly.</p
+            >
+          </div>
+          <div class="flex flex-col items-center mt-20 mr-12 text-center">
+            <div class="w-8 h-8 bg-purple-600"></div>
+            <div class="my-3 text-xl font-bold text-gray-800">Product Marketing</div>
+            <p class="leading-7 text-gray-800 w-60"
+              >A more intuitive way to present features of products.</p
+            >
+          </div>
+          <div class="flex flex-col items-center mt-20 mr-12 text-center">
+            <div class="w-8 h-8 bg-purple-600"></div>
+            <div class="my-3 text-xl font-bold text-gray-800">Enterprise Recruitment</div>
+            <p class="leading-7 text-gray-800 w-60"
+              >Get a more efficient and wider way of recruitment.</p
+            >
+          </div>
+          <div class="flex flex-col items-center mt-20 mr-12 text-center">
+            <div class="w-8 h-8 bg-purple-600"></div>
+            <div class="my-3 text-xl font-bold text-gray-800">School Education</div>
+            <p class="leading-7 text-gray-800 w-60"
+              >Virtual backgrounds make your speech more interesting.</p
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- section 4 -->
+    <div class="w-full">
+      <div class="container flex flex-col items-center mx-auto overflow-hidden">
+        <div class="text-5xl">How SOOM works?</div>
+        <div class="flex items-center justify-center mt-24">
+          <img
+            class="w-full h-auto"
+            src="https://soom.us/_nuxt/img/step-2.19f49c2.png"
+            alt=""
+            srcset=""
+          />
+          <ul class="flex flex-col w-4/12 ml-16">
+            <li class="mb-4">
+              <h4 class="text-xl font-bold text-gray-800">STEP1</h4>
+              <h5 class="my-2 text-lg text-purple-800">Select a presentation template</h5>
+              <p class="leading-6 text-gray-800"
+                >SOOM offers huge amount of fascinating presentation templates. Select a style that
+                suits you best and get started now.</p
+              >
+            </li>
+            <li class="mb-4">
+              <h4 class="text-xl font-bold text-gray-800">STEP2</h4>
+              <h5 class="my-2 text-lg text-purple-800">Replace the contents in template</h5>
+              <p class="leading-6 text-gray-800"
+                >SOOM offers huge amount of fascinating presentation templates. Select a style that
+                suits you best and get started now.</p
+              >
+            </li>
+            <li class="mb-4">
+              <h4 class="text-xl font-bold text-gray-800">STEP3</h4>
+              <h5 class="my-2 text-lg text-purple-800">Select a presentation template</h5>
+              <p class="leading-6 text-gray-800"
+                >SOOM offers huge amount of fascinating presentation templates. Select a style that
+                suits you best and get started now.</p
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- section 5 -->
+
+    <div class="relative mt-28 py-14">
+      <img
+        src="https://soom.us/_nuxt/img/experience-bg.9ead35b.jpg"
+        class="absolute top-0 left-0 z-0 w-full h-full"
+      />
+      <div class="container relative flex flex-col items-center mx-auto">
+        <div class="text-3xl text-gray-800">Experience SOOM Now</div>
+        <div class="mt-6 text-5xl font-bold leading-snug text-center text-gray-800"
+          >SOOM is to change your way of presentation from now on.</div
+        >
+        <button
+          class="flex items-center px-12 py-4 mt-20 text-white bg-purple-700 rounded-full hover:bg-purple-800 focus:outline-none"
+        >
+          <svg
+            class="mr-3 w-9"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+            />
+          </svg>
+          DOWNLOAD FOR WINDOWS
+        </button>
+        <span class="mt-3 text-sm font-light text-gray-400">MAC VERSION IS COMING SOON</span>
       </div>
     </div>
   </section>
