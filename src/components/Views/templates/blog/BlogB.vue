6e273b4b45a8ca11f39e7bc5f30b52c5bcce8a8c
laihua <zzw9059190@gmail.com>
2021-04-08 11:38:07 +0800

    feat: 新增模板

--- src/components/Views/templates/blog/BlogB.vue
+++ src/components/Views/templates/blog/BlogB.vue
@@ -1,206 +1,109 @@
 <template>
-  <section class="text-gray-600 body-font">
-    <div class="container px-5 py-24 mx-auto">
-      <div class="flex flex-wrap -m-4">
-        <div class="p-4 lg:w-1/3">
-          <div
-            class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative"
-          >
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
-              >CATEGORY</h2
-            >
-            <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3"
-              >Raclette Blueberry Nextious Level</h1
-            >
-            <p class="leading-relaxed mb-3"
-              >Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled
-              waistcoat.</p
-            >
-            <a class="text-green-500 inline-flex items-center"
-              >Learn More
-              <svg
-                class="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M5 12h14"></path>
-                <path d="M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
-            <div
-              class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4"
-            >
-              <span
-                class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200"
-              >
-                <svg
-                  class="w-4 h-4 mr-1"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                  <circle cx="12" cy="12" r="3"></circle></svg
-                >1.2K
-              </span>
-              <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                <svg
-                  class="w-4 h-4 mr-1"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
-                  ></path></svg
-                >6
-              </span>
-            </div>
-          </div>
-        </div>
-        <div class="p-4 lg:w-1/3">
-          <div
-            class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative"
-          >
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
-              >CATEGORY</h2
-            >
-            <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3"
-              >Ennui Snackwave Thundercats</h1
-            >
-            <p class="leading-relaxed mb-3"
-              >Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled
-              waistcoat.</p
-            >
-            <a class="text-green-500 inline-flex items-center"
-              >Learn More
-              <svg
-                class="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M5 12h14"></path>
-                <path d="M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
-            <div
-              class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4"
-            >
-              <span
-                class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200"
-              >
-                <svg
-                  class="w-4 h-4 mr-1"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                  <circle cx="12" cy="12" r="3"></circle></svg
-                >1.2K
-              </span>
-              <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                <svg
-                  class="w-4 h-4 mr-1"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
-                  ></path></svg
-                >6
-              </span>
-            </div>
-          </div>
-        </div>
-        <div class="p-4 lg:w-1/3">
-          <div
-            class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative"
-          >
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1"
-              >CATEGORY</h2
-            >
-            <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3"
-              >Selvage Poke Waistcoat Godard</h1
-            >
-            <p class="leading-relaxed mb-3"
-              >Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled
-              waistcoat.</p
-            >
-            <a class="text-green-500 inline-flex items-center"
-              >Learn More
-              <svg
-                class="w-4 h-4 ml-2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M5 12h14"></path>
-                <path d="M12 5l7 7-7 7"></path>
-              </svg>
-            </a>
-            <div
-              class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4"
-            >
-              <span
-                class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200"
-              >
-                <svg
-                  class="w-4 h-4 mr-1"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                  <circle cx="12" cy="12" r="3"></circle></svg
-                >1.2K
-              </span>
-              <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                <svg
-                  class="w-4 h-4 mr-1"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"
-                  ></path></svg
-                >6
-              </span>
-            </div>
-          </div>
-        </div>
+  <section class="container pt-20 mx-auto">
+    <div class="flex items-center justify-center mx-auto mb-36">
+      <div>
+        <h2 class="text-4xl font-bold text-gray-800">来画动画</h2>
+        <p class="mt-2 text-gray-600">像做PPT一样做视频 何止是快</p>
+        <ul class="mt-10 space-y-2">
+          <li class="flex items-center">
+            <span class="w-1.5 h-1.5 bg-blue-700 rounded-full mr-3"></span>
+            <span class="text-gray-600">海量正版可商用素材</span>
+          </li>
+          <li class="flex items-center">
+            <span class="w-1.5 h-1.5 bg-blue-700 rounded-full mr-3"></span>
+            <span class="text-gray-600">AI智能配音，多角色音库，配音so easy</span>
+          </li>
+          <li class="flex items-center">
+            <span class="w-1.5 h-1.5 bg-blue-700 rounded-full mr-3"></span>
+            <span class="text-gray-600">AI智能绘图，不会画画也能做动画</span>
+          </li>
+          <li class="flex items-center">
+            <span class="w-1.5 h-1.5 bg-blue-700 rounded-full mr-3"></span>
+            <span class="text-gray-600">智能字幕，一键转换，字幕不再让你烦心</span>
+          </li>
+        </ul>
+        <button
+          class="px-16 py-3 mt-4 text-sm text-white bg-blue-600 rounded-full focus:outline-none hover:bg-blue-500"
+          >进入</button
+        >
       </div>
+      <video
+        class="w-2/5 ml-20"
+        src="https://resources.laihua.com/2021-1-26/56f0d1dd-ec4e-4fe1-9f64-d47282961f97.mp4"
+        muted="muted"
+        autoplay
+        loop="loop"
+      ></video>
+    </div>
+    <div class="flex flex-row-reverse items-center justify-center mb-36">
+      <div>
+        <h2 class="text-4xl font-bold text-gray-800">来画视频</h2>
+        <p class="mt-2 text-gray-600">实时互动的视频演示</p>
+        <ul class="mt-10 space-y-2">
+          <li class="flex items-center">
+            <span class="w-1.5 h-1.5 bg-blue-700 rounded-full mr-3"></span>
+            <span class="text-gray-600">真人与演示内容实时互动，近距离打动观众</span>
+          </li>
+          <li class="flex items-center">
+            <span class="w-1.5 h-1.5 bg-blue-700 rounded-full mr-3"></span>
+            <span class="text-gray-600">自由调整人像的大小，支持一秒抠除背景</span>
+          </li>
+          <li class="flex items-center">
+            <span class="w-1.5 h-1.5 bg-blue-700 rounded-full mr-3"></span>
+            <span class="text-gray-600">画笔标注，观众的注意力由你掌控</span>
+          </li>
+          <li class="flex items-center">
+            <span class="w-1.5 h-1.5 bg-blue-700 rounded-full mr-3"></span>
+            <span class="text-gray-600">视频会议、学校教育、知识科普，做你所想</span>
+          </li>
+        </ul>
+        <button
+          class="px-16 py-3 mt-4 text-sm text-white bg-blue-600 rounded-full focus:outline-none hover:bg-blue-500"
+          >进入</button
+        >
+      </div>
+      <video
+        class="w-2/5 mr-36"
+        src="https://resources.laihua.com/2021-1-29/32c1d889-e819-4ae2-bb7a-4d882ab89db9.mp4"
+        muted="muted"
+        autoplay
+        loop="loop"
+      ></video>
+    </div>
+    <div class="flex items-center justify-center mb-36">
+      <div>
+        <h2 class="text-4xl font-bold text-gray-800">视频在线编辑</h2>
+        <p class="mt-2 text-gray-600">快速上手的视频处理工具</p>
+        <ul class="mt-10 space-y-2">
+          <li class="flex items-center">
+            <span class="w-1.5 h-1.5 bg-blue-700 rounded-full mr-3"></span>
+            <span class="text-gray-600">轻松快速剪辑视频片段</span>
+          </li>
+          <li class="flex items-center">
+            <span class="w-1.5 h-1.5 bg-blue-700 rounded-full mr-3"></span>
+            <span class="text-gray-600">手动添加字幕，语音识别字幕</span>
+          </li>
+          <li class="flex items-center">
+            <span class="w-1.5 h-1.5 bg-blue-700 rounded-full mr-3"></span>
+            <span class="text-gray-600">一键转换多种视频格式</span>
+          </li>
+          <li class="flex items-center">
+            <span class="w-1.5 h-1.5 bg-blue-700 rounded-full mr-3"></span>
+            <span class="text-gray-600">还有更多的全矩阵视频编辑工具</span>
+          </li>
+        </ul>
+        <button
+          class="px-16 py-3 mt-4 text-sm text-white bg-blue-600 rounded-full focus:outline-none hover:bg-blue-500"
+          >进入</button
+        >
+      </div>
+      <video
+        class="w-2/5 ml-20"
+        src="https://resources.laihua.com/2021-1-22/ed8c56d0-84e6-4cdb-89e8-8ffb8ffff5c1.mp4"
+        muted="muted"
+        autoplay
+        loop="loop"
+      ></video>
     </div>
   </section>
 </template>
